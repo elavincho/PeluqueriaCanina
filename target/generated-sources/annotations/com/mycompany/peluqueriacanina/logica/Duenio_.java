19f6e68b72e72680c4864ff770833ac2a19aef27
Mario David Gonzalez <elavincho@gmail.com>
2025-08-06 22:46:32 -0300

    Subo el proyecto

---
+++ target/generated-sources/annotations/com/mycompany/peluqueriacanina/logica/Duenio_.java
@@ -0,0 +1,15 @@
+package com.mycompany.peluqueriacanina.logica;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2025-07-09T17:31:12")
+@StaticMetamodel(Duenio.class)
+public class Duenio_ { 
+
+    public static volatile SingularAttribute<Duenio, String> celDuenio;
+    public static volatile SingularAttribute<Duenio, String> nombre;
+    public static volatile SingularAttribute<Duenio, Integer> id_duenio;
+
+}
---
+++ target/generated-sources/annotations/com/mycompany/peluqueriacanina/logica/Mascota_.java
@@ -0,0 +1,21 @@
+package com.mycompany.peluqueriacanina.logica;
+
+import com.mycompany.peluqueriacanina.logica.Duenio;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2025-07-09T17:31:12")
+@StaticMetamodel(Mascota.class)
+public class Mascota_ { 
+
+    public static volatile SingularAttribute<Mascota, Integer> num_cliente;
+    public static volatile SingularAttribute<Mascota, String> raza;
+    public static volatile SingularAttribute<Mascota, String> color;
+    public static volatile SingularAttribute<Mascota, Duenio> duenio;
+    public static volatile SingularAttribute<Mascota, String> alergico;
+    public static volatile SingularAttribute<Mascota, String> observaciones;
+    public static volatile SingularAttribute<Mascota, String> atencion_especial;
+    public static volatile SingularAttribute<Mascota, String> nombre;
+
+}
